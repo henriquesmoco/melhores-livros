7976f63e7f8246d27d8e38db9660e632bf8bcf66
Henrique Smoco <henriquesmoco@outlook.com>
2014-11-30 10:36:06 -0200

    Adicionado teste do MatrixBuilder com ballots da wikipedia

--- src/test/java/io/github/henriquesmoco/melhoreslivros/schulze/MatrixBuilderTest.java
+++ src/test/java/io/github/henriquesmoco/melhoreslivros/schulze/MatrixBuilderTest.java
@@ -1,6 +1,5 @@
 package io.github.henriquesmoco.melhoreslivros.schulze;
 
-import java.util.Arrays;
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
@@ -48,6 +47,36 @@ public class MatrixBuilderTest {
                 .addBallot(1, "X>Y>Z")
                 .addBallot(2, "Y>Z>X")
                 .addBallot(3, "Z>X>Y")
+                .build();
+        //System.out.println(Arrays.deepToString(matrix));
+        assertEquals(matrix, expectedMatrix);
+    }
+    
+    /**
+     * @see http://en.wikipedia.org/wiki/Schulze_method
+     */
+    @Test
+    public void buildMatrix_OfBallotsFromWikipedia_CreateMatrixCorrectly() {
+        MatrixBuilder matrixBuilder = new MatrixBuilder();
+        int[][] expectedMatrix = new int[][]{
+            //A, B, C, D, E
+            {0, 20, 26, 30, 22}, //A
+            {25, 0, 16, 33, 18}, //B
+            {19, 29, 0, 17, 24}, //C
+            {15, 12, 28, 0, 14}, //D
+            {23, 27, 21, 31, 0} //E
+
+        };
+        
+        int[][] matrix = matrixBuilder
+                .addBallot(5, "A>C>B>E>D")
+                .addBallot(5, "A>D>E>C>B")
+                .addBallot(8, "B>E>D>A>C")
+                .addBallot(3, "C>A>B>E>D")
+                .addBallot(7, "C>A>E>B>D")
+                .addBallot(2, "C>B>A>D>E")
+                .addBallot(7, "D>C>E>B>A")
+                .addBallot(8, "E>B>A>D>C")
                 .build();
         //System.out.println(Arrays.deepToString(matrix));
         assertEquals(matrix, expectedMatrix);
